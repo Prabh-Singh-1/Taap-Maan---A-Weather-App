6aaea09ea5de671e4df403b36a41687c2aa55b8a
Prabh-Singh-1 <workprabhsingh1.com>
2024-08-04 13:07:43 +0530

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: 'export',
-   webpack(config, options) {
-     config.module.rules.push({
-       test: /\.(mp4|webm|ogg|swf|ogv)$/,
-       use: [
-         {
-           loader: 'file-loader',
-           options: {
-             publicPath: '/_next/static/videos/',
-             outputPath: 'static/videos/',
-             name: '[name].[hash].[ext]',
-           },
-         },
-       ],
-     });
- 
-     return config;
-   },
- };
- 
- export default nextConfig;
+  output: 'export',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|swf|ogv)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/videos/', // Adjust if necessary
+            outputPath: 'videos/', // Adjust if necessary
+            name: '[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
